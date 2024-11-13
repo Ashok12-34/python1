b3aab0c12c9d91e2b4bc724b02a9f7db4bbe3d39
Ashok12-34 <ashok.reddy280303@gmail.com>
2024-11-13 14:57:30 +0530

    Add files via upload

---
+++ data analylist.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data=pd.read_csv("C://Users/win-10/Downloads/matches.csv")
+#print(data)
+#print(data.head())
+#print(data.columns)
+#print(data[data['Season']==2008].shape[0])
+#print(data[data['Season']==2009])
